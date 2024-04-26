fcc4aa7d3ac5ccb34d38e48efc08aabcc1e77111
larskvn <kvnyarbis@gmail.com>
2024-04-26 00:48:42 -0500

    update reame

--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, jsonify
-from routers.Persons import cv
-from utils.db import db
-app = Flask(__name__)
-
-
-app.register_blueprint(cv)
--- utils/db.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from mongoengine import connect
-from dotenv import load_dotenv
-import os
-load_dotenv()
-db=connect(host=os.getenv("BD_MONGO"))
-
